24eda2f265185e7ed327b973275f49a4b1357a1d
David Stone <david@stabbylambda.com>
2017-07-13 21:32:43 -0700

    Add ch1 from 1.1.1 to 1.1.4

---
+++ ch1.rkt
@@ -0,0 +1,57 @@
+#lang sicp
+
+; 1.1.1
+486
+(- 1000 334)
+(* 5 99)
+(/ 10 5)
+(+ 2.7 10)
+(+ 21 35 12 7)
+(* 25 4 12)
+(+ (* 3 5) (- 10 6))
+
+(+ (* 3 (+ (* 2 4) (+ 3 5))) (+ (- 10 7) 6))
+(+ (* 3
+      (+ (* 2 4)
+         (+ 3 5)))
+   (+ (- 10 7)
+      6))
+; 1.1.2
+(define size 2)
+
+size
+
+(* 5 size)
+
+(define pi 3.14159)
+(define radius 10)
+
+(* pi (* radius radius))
+
+(define circumference (* 2 pi radius))
+
+circumference
+
+; 1.1.3
+(* (+ 2 (* 4 6)) (+ 3 5 7))
+
+; 1.1.4
+(define (square x) (* x x))
+(square 21)
+(square (+ 2 5))
+(square (square 3))
+
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (f a)
+  (sum-of-squares (+ a 1) (* a 2)))
+
+(f 5)
+
+
+
+
+
+
